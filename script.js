b41293332e562bfa81eabbd77a71baa1dfc547b7
Ethan Hirst <ethan.hirst@powerhrg.com>
2024-03-01 18:28:57 -0500

    almost there

--- script.js
+++ script.js
@@ -2,47 +2,62 @@
 //tabs 
 
 function openCity(evt, cityName) {
-    // Declare all variables
-    var i, tabcontent, tablinks;
-  
-    // Get all elements with class="tabcontent" and hide them
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
-  } 
+  // Declare all variables
+  var i, tabcontent, tablinks;
 
-  var slideIndex = 2;
-  showDivs(slideIndex);
-  
-  function plusDivs(n) {
-	showDivs(slideIndex += n);
-  }
-  
-  function showDivs(n) {
-	var i;
-	var x = document.getElementsByClassName("mySlides");
-	if (n > x.length) {slideIndex = 1}
-	if (n < 1) {slideIndex = x.length} ;
-	for (i = 0; i < x.length; i++) {
-	  x[i].style.display = "none";
-	}
-	x[slideIndex-1].style.display = "block";
+  // Get all elements with class="tabcontent" and hide them
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
 
+  // Get all elements with class="tablinks" and remove the class "active"
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
 
-  function changeColor(id)
-{
-  		document.getElementById(id).style.color = "#ff0000"; // forecolor
-  		document.getElementById(id).style.backgroundColor = "#ff0000"; // backcolor
+  // Show the current tab, and add an "active" class to the button that opened the tab
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
+}
+
+  document.getElementById("defaultOpen").click();
+
+//slideshow (try add something like n=1 to start)
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
+
+
+
+//2 truths and a lie
+
+function changeColor(id) {
+  	document.getElementById(id).style.color = "#ff0000"; // forecolor
+  	document.getElementById(id).style.backgroundColor = "#ff0000"; // backcolor
+}
+
+function makeRed() {
+	document.getElementById("lie").style.color = "red";
+}
+
+function makeGreen() {
+	document.getElementById("truth").style.color = "green";
+}
+
+function makeGreenTwo() {
+	document.getElementById("correct").style.color = "green";
 }
