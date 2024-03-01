a8ed4b0a2c692f36d0717cb0f8a412211d41da00
Ethan Hirst <ethan.hirst@powerhrg.com>
2024-02-29 20:28:50 -0500

    add photos

--- script.js
+++ script.js
@@ -25,3 +25,24 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
   } 
 
+var indexValue = 1;
+showImg(indexValue)
+
+function side_slide(e) {
+    showImg(indexValue +=e);
+}
+
+function showImg(e) {
+    var i;
+    const img = document.querySelectorAll('img');
+    if(e > img.length) {
+        indexValue = 1
+    }
+    if(e < 1) {
+        indexValue = img.length
+    }
+    for(i = 0; i < img.length; i++) {
+        img[i].style.display = "none"
+    }
+    img[indexValue - 1].style.display = "block"
+}
