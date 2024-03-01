2de387330a21fd258f08c95907d587b7ae5451e3
Ethan Hirst <ethan.hirst@powerhrg.com>
2024-02-29 23:01:25 -0500

    slideshow

--- script.js
+++ script.js
@@ -25,24 +25,21 @@ function openCity(evt, cityName) {
     evt.currentTarget.className += " active";
   } 
 
-var indexValue = 1;
-showImg(indexValue)
 
-function side_slide(e) {
-    showImg(indexValue +=e);
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
 }
 
-function showImg(e) {
-    var i;
-    const img = document.querySelectorAll('img');
-    if(e > img.length) {
-        indexValue = 1
-    }
-    if(e < 1) {
-        indexValue = img.length
-    }
-    for(i = 0; i < img.length; i++) {
-        img[i].style.display = "none"
-    }
-    img[indexValue - 1].style.display = "block"
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
 }
